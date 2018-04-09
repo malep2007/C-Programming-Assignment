263d4bbc7b9a08e90102cc963edb7c7a330911f6
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-04-09 09:40:45 +0300

    Updated Readme file
    Signed-off-by: Ephraim Malinga <ephraim.malinga@gmail.com>

--- main.c
+++ main.c
@@ -1,23 +1,15 @@
+#include <ctype.h>
 #include <stdio.h>
 
 main(){
-    struct coord{
-        int x;
-        int y;
-    };
-    struct rectangle{
-        struct coord top_left;
-        struct coord bottom_right;
-    };
-    struct rectangle rect_one;
-//    rect_one.top_left.x = 0;
-//    rect_one.top_left.y = 4;
-    rect_one.top_left = {0, 4};
-    rect_one.bottom_right = {6, 0};
-
-    int length = (rect_one.bottom_right.x - rect_one.top_left.x);
-    int width = (rect_one.top_left.y - rect_one.bottom_right.y);
-    int ares = length * width;
-
+    char some_string[] = {};
+    for (int i = 0; i < 20; ++i) {
+        scanf("%s", some_string);
+        if (some_string[i] == EOF)
+        {
+            break;
+        }
+    }
+    putchar(some_string);
 
 }
