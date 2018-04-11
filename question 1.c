973201ccf2b1a70824c8bc84cf96f0067a4c3de1
SsemagandaEdrine <38284878+SsemagandaEdrine@users.noreply.github.com>
2018-04-11 20:19:52 +0300

    SSEMAGANDA EDRINE
    17/U/10152/EVE
    assignment

---
+++ question 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int x=1;
+
+ main()
+{
+    if(x==1)
+
+    printf("x equals 1");
+
+    else
+
+    printf("x does not equals 1");
+
+    return 0;
+}
---
+++ question 3.c
@@ -0,0 +1,48 @@
+Questio 3
+
+part1
+
+long valuesp[50];
+
+
+
+part2
+
+long values[49]=123456;
+
+
+
+part3
+
+x is equal to 100 when the statement is complete
+
+
+
+part4
+
+the value of ctr is 11 when the statement is complete
+
+
+
+
+part5
+
+#include <stdio.h>
+main()
+{
+int count=1;
+while(count<=100)
+    {
+    printf("%d",count);
+    count +=3;
+    }
+}
+
+
+
+
+
+
+part6
+
+The code under the for loop is always executed once, outside of the for loop,because of the semicolon terminating the for loop.
