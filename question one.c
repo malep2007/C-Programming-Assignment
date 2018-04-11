201e852037f6669226c20d3aeda92dc5368da4d6
MusinguziDenis <38283521+MusinguziDenis@users.noreply.github.com>
2018-04-11 15:54:26 +0300

    Create question one.c

---
+++ question one.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int x=1;
+main()
+{
+    if(x==1)
+    {
+      printf("x equals one");
+    }
+    else
+    {
+      printf("x doesnot equal one");
+    }
+    return 0;
+}
+   
+   
