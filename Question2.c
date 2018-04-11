31bacbbd707050c0b99fae2883e9f5c8e5a12543
oriankeith1 <38229658+oriankeith1@users.noreply.github.com>
2018-04-11 20:58:08 +0300

    SSEMAMBYA ORIAN
    17/U/1113

---
+++ Question2.c
@@ -0,0 +1,28 @@
+Question 2
+
+part 1
+
+#ifndef DO_IT_H
+#define DO_IT_H
+
+float do_it (char a , char b, char C);
+#endif //DO_IT-H
+
+
+
+
+
+part2
+
+#ifndef PRINT_A_NUMBER_H
+#define PRINT_A_NUMBER_H
+
+void print_a_numver(int x);
+#endif // PRINT_A_NUMBER_H
+
+
+Part3
+
+The print_msg() function is declared as a void; However it returns a value thus the return statement should be removed.
+
+The second problem is the call to print_msg() passes the parameter (a string)
