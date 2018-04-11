88fb85fdb788357c53aafd9085050783912d5249
BYOONANIWE-Philip-Winner <38228049+BYOONANIWE-Philip-Winner@users.noreply.github.com>
2018-04-11 19:20:01 +0300

    Question 3
    These are the solutions for question 3

---
+++ Question 3.c
@@ -0,0 +1,18 @@
+//part_1
+long arr[50];
+
+//part_2
+arr[49]= 123.456;
+
+//part_3
+The value of ctr when the statement is complete is 8
+
+//part_4
+int i=1;
+while(i<=100){
+printf("%d", i);
+i +=3;
+}
+
+//part_5
+There should be no semi-colon terminating the for statement.
