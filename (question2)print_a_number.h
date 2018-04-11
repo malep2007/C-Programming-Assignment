1d4f7448ea1126a8a61ef370aa6a9bec4f7de2c1
AsheruraEmmanuel <38238823+AsheruraEmmanuel@users.noreply.github.com>
2018-04-11 16:59:53 +0300

    Add files via upload

---
+++ (question2)print_a_number.h
@@ -0,0 +1,5 @@
+/*heder file for a function print_a_number() that takes a single type int argument and returns a type float to the callig program*/
+#ifndef PRINT_A_NUMBER_H
+#define PRINT_A_NUMBER_H
+viod print_a_number(int x);
+#endif
