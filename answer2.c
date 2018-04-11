fef7f002d507eef41c4b636f262a2bed947758f6
katongole shafic <38281450+katongole32@users.noreply.github.com>
2018-04-11 17:36:40 +0300

    question2 answer

---
+++ answer2.c
@@ -0,0 +1,29 @@
+/*header file for a function do_ it that takes in three type character and returns a type float to the calling program*///
+(do_ it.h)
+#ifndef DO_ IT_H
+#define DO_IT_H
+
+float do_it(char a,char b,char c);
+
+#endif
+
+
+
+
+
+
+/*header file for a function print_a_number()that takes on a single type int argument and returns a type float to the calling program*/
+(print_a_number.h)
+#ifndef PRINT_A_NUMBER_H
+#define PRINT_A_NUMBER_H
+
+void print_a_number(int x);
+
+
+#endif
+
+
+
+
+error in the program
+The print_msg() function is not supposed to take any arguments but the main function calls it with a string argument.
