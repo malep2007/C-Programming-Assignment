06d1f710cfdf9dcf3a49403213e6e44c9fc57c7f
AinemukamaDaniel <38215582+AinemukamaDaniel@users.noreply.github.com>
2018-04-11 07:58:04 -0700

    solution to question

---
+++ Question 2.c
@@ -0,0 +1,15 @@
+float do_it(char x,char y,char z)
+
+void print_a_number( int a_number)
+
+#include<stdio.h>
+void print_msg(void);
+int main(void)
+{
+    print_msg();
+    return 0;
+}
+void print_msg(void)
+{
+    puts("This is a message to print");
+}
