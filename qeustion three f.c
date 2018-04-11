60a7c4d4ec04b060324e3cb9d881107546358822
AsheruraEmmanuel <38238823+AsheruraEmmanuel@users.noreply.github.com>
2018-04-11 18:26:47 +0300

    Delete qeustion three f.c

--- qeustion three f.c
+++
@@ -1,3 +0,0 @@
-for(counter =1;counter<MAXVALUES; counter++ );/*the for loop is terminated*/
-printf("\ncounter=%d",counter);
-/*the dor loop is terminated by";" hence the statement under it is executed once*/
