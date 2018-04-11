a83a6633c559b541214c93c7da3a00dc3cbd1e68
Masombo-Simon <38211528+Masombo-Simon@users.noreply.github.com>
2018-04-11 02:29:40 -0700

    Add files via upload

---
+++ question 4/main.c
@@ -0,0 +1,36 @@
+void addarrays(int A1[], int A2[], int D[], int SIZE)
+{
+  for(int i=0;i<SIZE;i++)
+  {
+   D[i] = A1[i] + A2[i];
+  }
+
+}
+
+
+
+
+
+
+#include <stdio.h>
+int *addarrays(int array1[], int array2[], int SIZE);
+main(){
+     int array1[] = {2,5,3,22,6};
+     int array2[] = {13,143,11,10,121};
+     int *array3 = addarrays(array1, array2, 5);
+     for(int i=0;i<5;i++) {
+        printf("%d \n", array3[i]);
+      }
+}
+
+
+
+
+
+
+     int *addarrays(int array1[], int array2[], int length){
+     int *destination_array = malloc(length * sizeof(int));
+     for(int i=0;i<length;i++){
+     destination_array[i] = array1[i] + array2[i];
+     }
+     return destination_array;
