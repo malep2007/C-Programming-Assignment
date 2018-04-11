744573b139a8cfc4264790fd21b7d11be80df181
nanzirigloria <38278764+nanzirigloria@users.noreply.github.com>
2018-04-11 17:20:03 +0300

    Assignment
    NANZIRI MARIA GLORIA
    Reg no: 17/U/8805/PS
    Std No: 217003993

---
+++ NANZIRI MARIA GLORIA/Question Three.c
@@ -0,0 +1,23 @@
+long ellarian[50];
+ellarian[49] = 123.456;
+
+for (x = 0); x < 100; x++)
+// the value of x when the statement is complete is 100
+
+for(ctr = 2; ctr < 10; ctr += 3)
+ //the value of ctr when the statement is complete is 11
+
+//while statement to count from 1 to 100 by 3s
+ int i;
+ i = 0;
+
+ while(i<101)
+{
+     printf("%d\n", i);
+     i += 3;
+}
+
+
+for (counter = 1; counter < MAXVALUES; counter++ );
+printf("\nCounter = %d", counter);
+//this is nothing wrong with with the above code fragment unless it was required to print the statement multiple times
