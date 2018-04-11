c8735fbfe38142bcef9bfe5c51b4440302f5be8f
MPAWULO-RONALD-ISAIAH <38224116+MPAWULO-RONALD-ISAIAH@users.noreply.github.com>
2018-04-11 08:03:08 -0700

    FOR MPAWULO RONALD ISAIAH 17/U/569
    MPAWULO RONALD ISAIAH 17/U/569 BSC TELECOMMUNICATIONS ENGINEERING - Programming assignment

---
+++ MPAWULO-RONALD-ISAIAH/q3.c
@@ -0,0 +1,17 @@
+//MPAWULO RONALD ISAIAH BSc TELECOMMUNICATIONS ENGINEERING 17/U/569
+long array[50];
+
+long array[49] = 123.456;
+
+for (x = 0; x < 100; x++) // x = 100
+
+for (ctr = 2; ctr < 10; ctr+=3) // ctr value = 11
+
+int counter, c = 1;
+while ( c <= 100) {
+  counter = i;
+  c += 3;
+}
+ /*(f) code fragment*/
+for (counter = 1; counter < MAXVALUES; counter++ ); //there are no errors in the code
+printf("\nCounter = %d", counter);
