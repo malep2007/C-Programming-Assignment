5228d3ea2d70965225972dc99af1026d03f777f1
BYOONANIWE-Philip-Winner <38228049+BYOONANIWE-Philip-Winner@users.noreply.github.com>
2018-04-11 19:18:55 +0300

    Question 2c
    This is the solution for question 2c

---
+++ question 2c.c
@@ -0,0 +1,4 @@
+//Answer
+
+From the function prototype, it is evident that the function print_msg() does not take any arguments.
+However,the function has been called with a string argument in the main function.
