7d0e6d038e88101da6dd7fc9c4480d4f38808ff7
RUKUNDOMUTANAPETER <rukundopeterclaver@gmail.com>
2018-04-11 10:51:40 -0700

    Add files via upload

---
+++ QN 3 A.c
@@ -0,0 +1,13 @@
+//A declaration for an array that will hold 50 type long values
+long values[50];
+
+//a statement that assigns the value 123,456 to the 50th element in the array from the above equation
+long values[49] = 123456;
+
+//x is equal to 100 when the statement is complete
+
+//The value of ctr is 11 when the statement is complete
+
+// What is wrong with the following code fragment (MAXVALUES is not the problem !)
+
+//The code under the for loop is always executed once, outside of the for loop, because of the semicolon terminating the for loop.
