615df70b4e72ce0c3e6f75bc048cb32591e20b84
BagumaKevin <38146177+BagumaKevin@users.noreply.github.com>
2018-04-12 02:09:11 +0300

    I have made some changes on question 2 to come up with this final solution

--- Question 2.c
+++ Question 2.c
@@ -1,6 +1,10 @@
-float do_it(char q,char p,char r)
+float do_it(char x,char y,char z)
 
-void print_a_number( int a_number)
+void print_a_number( int m)
+    
+    On the third line, the space between print_msg function should be empty that is: print_msg();
+This is because on the function declaration they used void as in "void print_msg(void);" implyimg that print_msg function should not take on any argument.
+    When defining the function, another function name was used that is "void print_msq" instead of "void print_msg". Hence the correct program would have been as follows;
 
 #include<stdio.h>
 void print_msg(void);
