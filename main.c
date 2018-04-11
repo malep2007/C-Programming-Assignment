7abb5189eb18fc285ad2e89fd05e985a05c2541b
SsenyongaDavid <38104274+SsenyongaDavid@users.noreply.github.com>
2018-04-11 22:01:40 +0300

    ANSWER 3

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-main(){
-    char some_string[] = {};
-    for (int i = 0; i < 20; ++i) {
-        scanf("%s", some_string);
-        if (some_string[i] == EOF)
-        {
-            break;
-        }
-    }
-    putchar(some_string);
+int main()
+{
+  // PART A
+  int marks[50];
 
+  // PART B
+  marks[49]=123.456
+
+  // PART C
+   X will have values from 0 to 99 with an increment of 1
+
+   // PART D
+   ctr = {2, 5, 8}
+
+   // PART E
+   Termination of the loop with a semicolon
 }
