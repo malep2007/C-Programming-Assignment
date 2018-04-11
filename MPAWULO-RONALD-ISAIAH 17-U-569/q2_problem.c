16516b86e0fadfe2d15cdf90528449d6286fff51
MPAWULO-RONALD-ISAIAH <38224116+MPAWULO-RONALD-ISAIAH@users.noreply.github.com>
2018-04-11 08:24:04 -0700

    Update q2_problem.c
    slight change

--- MPAWULO-RONALD-ISAIAH 17-U-569/q2_problem.c
+++ MPAWULO-RONALD-ISAIAH 17-U-569/q2_problem.c
@@ -1,11 +1,11 @@
-// MPAWULO RONALD ISAIAH BSc TELECOMMUNICATIONS ENGINEERING 17/U/569
+m// MPAWULO RONALD ISAIAH BSc TELECOMMUNICATIONS ENGINEERING 17/U/569
 #include <stdio.h>
 void print_msg( void );
 main(){
   print_msg("This is a message to print");//The print_msg function does not take arguments arguments.
   return 0;
 }
-void print_msq( void )  //This is supposed to be print_msg not print_q
+void print_msq( void )  //This is supposed to be print_msg not print_msq
 {
   puts("This is a message to print");
   return 0; // This is unexpected because the return type was specified as 'void'
