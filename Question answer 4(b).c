7a117e5eba201a2dcf8f8e1af04d23d7d5860f6b
luhoni <38288675+luhoni@users.noreply.github.com>
2018-04-12 00:17:19 +0300

    Update Question answer 4(b).c

--- Question answer 4(b).c
+++ Question answer 4(b).c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int i;
-int *addarrays( int elements1[],int elements2[],int SIZE);
+int *addarrays( int value1[],int value2[],int SIZE);
 
 main()
 {
-    int elements1[]={21,45,87,56,45};
-    int elements2[]={43,65,77,41,21};
+    int value1[]={21,45,87,56,45};
+    int value2[]={43,65,77,41,21};
 
-    int *total_elements=addarrays(elements1,elements2,5);
+    int *total_values=addarrays(elements1,elements2,5);
     for( i=0;i<5;i++){
-        printf("%d\n",total_elements[i]);
+        printf("%d\n",total_values[i]);
     }
 }
 
-int *total_elements(int elements1[],int elements2[],int length){
-int *total_elements=malloc(length*sizeof(int));
+int *total_values(int value1[],int value2[],int length){
+int *total_values=malloc(length*sizeof(int));
    for( i=0;i<length;i++){
-    total_elements[i]=elements1[i]+elements2[i];
+    total_values[i]=value1[i]+value2[i];
    }
-   return total_elements;
+   return total_values;
 }
