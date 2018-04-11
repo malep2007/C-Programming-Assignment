236a34c884a546d2107fa01cf2a31a01ee44fa5f
ssegawaderick <38277429+ssegawaderick@users.noreply.github.com>
2018-04-11 16:55:00 +0300

    Add files via upload
    SSEGAWA DERICK KITYO 17/U/10095/PS CMP

---
+++ q1.c
@@ -0,0 +1,14 @@
+/* SSEGAWA DERICK KITYO 17/U/10095/PS CMP */
+/*running programme*/
+#include <stdio.h>
+int x = 1;
+int main()
+{
+  if( x == 1){
+    printf(" x equals 1" );
+  }
+  else {
+    printf(" x does not equal 1");
+  }
+  return 0;
+}
---
+++ q3.c
@@ -0,0 +1,17 @@
+
+long array[50];
+
+long array[49] = 123.456;
+
+for (x = 0; x < 100; x++) /*x = 100*/
+
+for (ctr = 2; ctr < 10; ctr+=3) /* ctr value = 11*/
+
+int counter, c = 1;
+while ( c <= 100) {
+  counter = i;
+  c += 3;
+}
+ /*(f) code fragment*/
+for (counter = 1; counter < MAXVALUES; counter++ ); /* no errors*/
+printf("\nCounter = %d", counter);
