f63ca6d2ad83e8f9c1430aa02138a307d07fada7
AsheruraEmmanuel <38238823+AsheruraEmmanuel@users.noreply.github.com>
2018-04-11 18:24:22 +0300

    Add files via upload

---
+++ qeustion three f.c
@@ -0,0 +1,3 @@
+for(counter =1;counter<MAXVALUES; counter++ );/*the for loop is terminated*/
+printf("\ncounter=%d",counter);
+/*the dor loop is terminated by";" hence the statement under it is executed once*/
