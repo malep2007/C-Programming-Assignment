7d98f5416636f2dd6f9bde3f326124712e25a1e0
BagumaKevin <38146177+BagumaKevin@users.noreply.github.com>
2018-04-12 03:22:57 +0300

    Update Question 3.c

--- Question 3.c
+++ Question 3.c
@@ -10,7 +10,7 @@ When the statement is complete, the values of ctr are 2, 5 and 8.(ctr starts at 
 int x=1;
 while( x<=100)
 {
-  printf("%d/n",x);   //my PC can`t put the backslash symbol, that`s why i have used the forward slash
+  printf("%d\n",x);  
   x+=3;
 }
 
