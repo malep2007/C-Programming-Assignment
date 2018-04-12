b5064f6b14da418c626d3cad250e5ef7abead1ad
arthurolowo <arthurmarshal5@gmail.com>
2018-04-12 11:20:36 +0300

    question 4

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 /* OLOWO ARTHUR 16/U/10978/PS */
 #include <stdio.h>
-//print_a_number_h
+#include <stdlib.h>
+int i=0;
 int main()
+{
+    void AddArrays(int array1[], int array2[], int destination_array[], int size)
     {
-    #ifndef PRINT_A_NUMBER_H
-    #define PRINT_A_NUMBER_H
-    void print_a_number(int x);
-    #endif
+    for(i=0;i<size;i++)
+    {
+    destination_array[i] = array1[i] + array2[i];
+}
+}
+
 }
