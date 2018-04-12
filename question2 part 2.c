aab8fbc2e276cd9e1d84a6d3739f5e4f88b9ae8c
Ssempiiraeddy <38309035+Ssempiiraeddy@users.noreply.github.com>
2018-04-12 20:28:10 +1400

    Add files via upload
    Ssempiira Edward - 17/U/10196/Ps

---
+++ question2 part 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_msg(int a)
+{
+
+}
+int main()
+{
+    print_msg(/*integer to be entered*/);
+    return 0;
+}
---
+++ question3 part1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long semps[50];
+    semps[49]=123.456;
+
+    return 0;
+}
