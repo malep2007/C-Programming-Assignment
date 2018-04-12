d96c09018f61e72d4864578ad26e5b5f0d167a2c
BagumaKevin <38146177+BagumaKevin@users.noreply.github.com>
2018-04-12 03:08:56 +0300

    I have made changes on question 3 and updated it from branch qn3

--- Question 3.c
+++ Question 3.c
@@ -2,14 +2,17 @@ long array[50];
 
 array[49]=123.456;
 
-When the statement is complete, x equals 100.
+When the statement is complete,the values of x are from 0 to 99.
 
-When the statement is complete, ctr equals 11.(ctr starts at 2 and is incremented by 3 while it is less than 10)
+When the statement is complete, the values of ctr are 2, 5 and 8.(ctr starts at 2 and is incremented by 3 while it is less than 10, the final value being 8)
 
 
 int x=1;
 while( x<=100)
-x+=3;
+{
+  printf("%d/n",x);   //my PC can`t put the backslash symbol, that`s why i have used the forward slash
+  x+=3;
+}
 
 
-The semicolon does not belong at the end of the for statement.
+The semicolon at the end of the for statement must not exist there ie a for statement is not terminated.
