0f61cb71e927007cdabafdc320a9c0115dbcab6b
elizabeth-kamayangi <kamayangie@gmail.com>
2018-04-12 10:16:40 +0300

    Add files via upload

---
+++ question three.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main(){
+    int count = 1;
+    while(count<=100){
+        printf("%d",count);
+        count +=3;
+    }
+}
+
+Write a declaration for an array that will hold 50 type long values  
+Answer: long values[50];
+  
+Show a statement that assigns the value 123,456 to the 50th element in the array from the above equation  
+Answer: long values[49] = 123456; 
+  
+What is the value of x when the foolowing statement is complete  
+Answer: x is equal to 100 when the statement is complete
+  
+What is the value of ctr when the following statement is complete  
+Answer: The value of ctr is 11 when the statement is complete
+
+What is wrong with the following code fragment (MAXVALUES is not the problem !)  
+Answer: The code under the for loop is always executed once, outside of the for loop, because of the semicolon terminating the for loop. 
