cce40b25dac9045ad5491942cd0b6a28d99f881a
arthurolowo <arthurmarshal5@gmail.com>
2018-04-12 11:18:25 +0300

    question 2

--- main.c
+++ main.c
@@ -1,16 +1,10 @@
-/* OLOWO ARTHUR 16/U/10978/PS */  
-#include <stdio.h> 
-
-int x=1;
+/* OLOWO ARTHUR 16/U/10978/PS */
+#include <stdio.h>
+//print_a_number_h
 int main()
-
-{
-//int x = 1;
-
-if(x==1)
-printf ("x equals 1");
-else
-printf ("x does not equal 1");
-return 0;
+    {
+    #ifndef PRINT_A_NUMBER_H
+    #define PRINT_A_NUMBER_H
+    void print_a_number(int x);
+    #endif
 }
-
