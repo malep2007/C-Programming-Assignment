5ce7624b680e4ab225390248d860580b73bba91b
Roger-Kle <38290527+Roger-Kle@users.noreply.github.com>
2018-04-12 11:54:28 +0300

    Add files via upload
    Kabaale Rogers
    17/U/4254/PSA
    BELE

---
+++ q2_problem.c
@@ -0,0 +1,20 @@
+//Question 2a
+float do_it(char a,char b,char c);
+
+
+//Question 2b
+void print_a_number(int m);
+
+//Question 2c
+
+#include <stdio.h>
+void print_msg( void );
+main(){
+  print_msg("This is a message to print");//The print_msg function does not take arguments on it.
+  return 0;
+}
+void print_msq( void )  //This is supposed to be print_msg not print_msq
+{
+  puts("This is a message to print");
+  return 0; // This is unexpected because the return type was already specified as 'void'
+}
---
+++ q4b.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int results[] = {};
+int *addarrays(int array1[], int array2[], int n);
+
+int main(void) {
+  int i,n;
+  printf("Enter the size of the arrays: \n");
+  scanf("%d",&n);
+  int array_first[n], array_second[n];
+  printf("Enter First array: \n");
+
+  for ( i = 0; i < n; i++) {
+    scanf("%d", &array_first[i]);
+  }
+
+  printf("Enter Second array: \n");
+
+  for ( i = 0; i < n; i++) {
+    scanf("%d", &array_second[i]);
+  }
+
+  int *y;
+  y = addarrays(array_first, array_second, n);
+   printf("array one: ");
+    for (i=0; i < n ; i++)  {
+  printf("%d,", array_first[i]);
+  }
+  printf("\n array two: ");
+  for (i=0; i < n; i++){
+  printf("%d,", array_second[i]);
+  }
+  printf("\nresults array: ");
+  for (i=0; i < n; i++){
+  printf("%d,", y[i]);
+  }
+  return 0;
+}
+
+int *addarrays(int array1[], int array2[], int n) {
+  int i = 0;
+  for (i = 0; i < n; i++) {
+    results[i] = array1[i] + array2[i];
+  }
+  int *z;
+  z = results;
+  return z;
+}
