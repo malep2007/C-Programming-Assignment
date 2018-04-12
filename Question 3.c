5781fcd208fa2d5a3160982f52d4f18d5b306292
Colton2137 <samulamichaelc@gmail.com>
2018-04-12 10:55:00 +0300

    Question 3

---
+++ Question 3.c
@@ -0,0 +1,28 @@
+
+long values[50]
+
+long values[49] = 123.456
+
+final value of x is 100
+
+final value of ctr is 11
+
+
+
+
+
+
+#include <stdio.h>
+
+main(){
+ /*let count variable be x*/
+int x = 1;
+while(x <= 100){
+    printf("%d ", x);
+    x += 3;
+    }
+}
+
+
+
+the problem is that the for loop has no parenthesis to indicate a statement and it is terminated at a wrong location
