b4339b0c478f04061a8f7f43bb7695765f3bafe0
agnesnakalembe <38295251+agnesnakalembe@users.noreply.github.com>
2018-04-12 11:52:40 +0300

    Question 1

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-#include <ctype.h>
+//NAME: NAKALEMBE AGNES
+//REG_NO: 16/U/811
+//*STUDENT NO:216001305S
+//COURSE: COMPUTER ENGINEERING II
+
 #include <stdio.h>
 
-main(){
-    char some_string[] = {};
-    for (int i = 0; i < 20; ++i) {
-        scanf("%s", some_string);
-        if (some_string[i] == EOF)
-        {
-            break;
-        }
-    }
-    putchar(some_string);
+int x=1;
+int main()
 
+{
+//int x = 1;
+if(x==1)
+printf ("x equals 1");
+else
+printf ("x does not equal 1");
+return 0;
 }
+
