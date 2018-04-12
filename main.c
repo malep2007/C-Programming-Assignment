63220a769b852baf7d3a582b7231e053fab5de36
TANYOOMWARICHARD <38227200+TANYOOMWARICHARD@users.noreply.github.com>
2018-04-12 11:43:29 +0300

    TANYOOMWA RICHARD 17/U/1913

--- main.c
+++ main.c
@@ -1,15 +1,37 @@
-#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
 
-main(){
-    char some_string[] = {};
-    for (int i = 0; i < 20; ++i) {
-        scanf("%s", some_string);
-        if (some_string[i] == EOF)
-        {
-            break;
+
+void addarrays(int arrayone[5],int arraytwo[5]){
+        int i;
+        int sum[5];
+        printf("\nTotal arrays\n");
+        for(i=0;i<5;i++){
+        sum[i] = arrayone[i] + arraytwo[i];
+        printf(" %d\t",sum[i]);
         }
-    }
-    putchar(some_string);
-
+        return;
 }
+int main()
+{
+    int m;
+    int arrayA[5] = {23,24,31,40,5};
+    int arrayB[5] = {21,22,23,24,25};
+    printf("first array\n");
+    for(m=0;m<5;m++){
+        printf(" %d\t",arrayA[m]);
+    }
+     printf("\nsecond array\n");
+     for(m=0;m<5;m++){
+        printf(" %d\t",arrayB[m]);
+    }
+
+     addarrays(arrayA,arrayB);
+
+     return 0;
+}
+
+
