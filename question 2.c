0772f4c4f3aa443bfef09b68ec9de8982add80f4
nyafgurl <hiztachmaggie@gmail.com>
2018-04-12 09:57:18 +0300

    Question 2

---
+++ question 2.c
@@ -0,0 +1,5 @@
+a) float do_it(char argument1, char argument2, char argument3);
+
+b) void print_a_number(int number);
+
+c) the main is not supposed to be returning anything but it returns an integer. the function print_message has a void return type yet the funtion definition is returning an integer
