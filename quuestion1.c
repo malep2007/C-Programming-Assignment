fa4f9119c41c19dc2b80dff22afde2b235776f3d
Obilangole <38161490+Obilangole@users.noreply.github.com>
2018-04-12 18:40:32 +1400

    Add files via upload
    Obilangole Edgar - 17/U/973

---
+++ quuestion1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x = 1;
+    if(x==1)
+    {
+        printf("X equals 1");
+    }
+    else
+    {
+        printf("X doesnot equal 1");
+    }
+    return 0;
+}
