017d3ae0483addda912ecfd13ac40314b3f5e021
nyafgurl <hiztachmaggie@gmail.com>
2018-04-12 09:58:29 +0300

    Question 3

---
+++ question 3.c
@@ -0,0 +1,19 @@
+a) long val[50];
+
+b) val[49]=123.456;
+
+c) 0-99
+
+d) 2,5,8
+
+e)
+int count=1;
+while(count<=100){
+	count+=3;
+}
+
+f)
+the counter address in the print statement is not properly referenced i.e. missing an ampersand(&)
+the code under loop is always executed once, outside of the loop, beacause of the semicolon terminating the loop.
+
+
