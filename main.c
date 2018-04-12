bfb3b6e9877073366e82529801a3fc0f8808eacc
SaraAhere <38314182+SaraAhere@users.noreply.github.com>
2018-04-12 12:03:13 +0300

    Question 2

--- main.c
+++ main.c
@@ -1,18 +1,9 @@
-//NAME: AHEREZA SARAH
-//REG_NO: 16/U/35
-//COURSE: COMPUTER ENGINEERING II
-
 #include <stdio.h>
-
-int x=1;
+//print_a_number_h
 int main()
-
-{
-//int x = 1;
-if(x==1)
-printf ("x equals 1");
-else
-printf ("x does not equal 1");
-return 0;
+    {
+    #ifndef PRINT_A_NUMBER_H
+    #define PRINT_A_NUMBER_H
+    void print_a_number(int x);
+    #endif
 }
-
