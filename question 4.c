6b96ae423316018363b70efcabf2b4788a1d8391
nyafgurl <hiztachmaggie@gmail.com>
2018-04-12 09:59:25 +0300

    Question 4

---
+++ question 4.c
@@ -0,0 +1,35 @@
+void addarrays(int array1[], int array2[]){
+int det;
+int n=sizeof(array1[])/size of (det);
+int m=sizeof(array2[])/size of (det);
+int result[n];
+if(n=m){
+int counter;
+for(counter=0;counter<n;counter++){
+           result[counter]=array1[counter]+array2[counter];
+           print("%d\n", &result[counter]);
+}
+}else{
+printf("Arrays are unequal in size");
+}
+}
+
+
+#(b)
+int *result[](int array1[], int array2[]{
+int det;
+int n=sizeof(array1[]/sizeof(det);
+int m=sizeof(array2[]/sizeof(det);
+int result[n];
+if (n==m){
+int counter;
+for(counter=0;counter<n;counter++){
+           result[counter]=array1[counter]+array2[counter];
+           printf("%d\n", &result[counter]);
+}
+return result;
+}else{
+printf("Arrays are unequal in size");
+reurn null;
+}
+}
