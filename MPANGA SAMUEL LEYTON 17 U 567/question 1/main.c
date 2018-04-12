a084547d4ee68fa8ebe3b56b7abba29835bc3ac5
MPANGA SAMUEL LEYTON <33018745+leytec@users.noreply.github.com>
2018-04-12 11:27:09 +0300

    Add files via upload

---
+++ MPANGA SAMUEL LEYTON 17 U 567/question 1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x= 0;
+    if( x == 1)
+    {
+        printf(" x equals 1");
+    }
+    else
+    {
+         printf("x does not equal 1");
+    }
+
+    return 0;
+}
---
+++ MPANGA SAMUEL LEYTON 17 U 567/quetion 2/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    #include <stdio.h>
+
+void print_msg( void );
+
+main(){
+
+  print_msg("This is a message to print");//The print_msg function does not take arguments arguments.
+
+  return 0;
+
+}
+
+void print_msq( void )  //This is supposed to be print_msg not print_msq
+
+{
+
+  puts("This is a message to print");
+
+  return 0; // This is unexpected because the return type was specified as 'void'
+
+}
